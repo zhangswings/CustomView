a1610d8ca3ed08790400f8c14cb1b09777faaf49
kira <644904088@qq.com>
2018-10-27 19:09:55 +0800

    Initial commit

---
+++ app/src/main/java/win/night/customview/view/CircleView.java
@@ -0,0 +1,51 @@
+package win.night.customview.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 自定义circleView
+ */
+public class CircleView extends View {
+
+    private int mDefaultColor = Color.RED;
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public CircleView(Context context) {
+        super(context);
+        //init default attrs
+        init();
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+
+    private void init() {
+        mPaint.setColor(mDefaultColor);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int width = getWidth();
+        int height = getHeight();
+
+        int radius = Math.min(width, height) / 2;
+        canvas.drawCircle(width / 2, height / 2, radius, mPaint);
+    }
+}
